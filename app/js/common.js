18bca00f0f4e0d0239bfb664793f95cd78a51fa6
mrikaev <mrikaev@mwpost.ru>
2020-03-21 18:09:16 +0300

    Верхняя панель и меню для полной версии

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,11 @@
 $(function() {
-
-	// Custom JS
-
+    $('.dropdown').hover(function() {
+        let inner = $(this).find('.dropdown__inner')
+        console.log('hover');
+        inner.stop(true, true).slideDown(100);
+    }, function() {
+        let inner = $(this).find('.dropdown__inner')
+        console.log('hover out');
+        inner.stop(true, true).slideUp(100);
+    });
 });
